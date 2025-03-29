5e0273bbb822e81bb6b51cb811abb9a1535c26b3
Armando Andrés Meabe <armando.meabe@outlook.com>
2025-03-29 16:42:30 -0300

    Commit inicial

---
+++ Solucion Limpia/MAVISFML/MAVISFML.cpp
@@ -0,0 +1,21 @@
+//////Bibliotecas//////
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+///Punto de entrada a la aplicación 2 ///
+
+
+int main() {
+	//Creamos la ventana
+	sf::RenderWindow App(sf::VideoMode(1920, 1080, 32),
+		"Que ventana horrible");
+	// Loop principal
+	while (App.isOpen())
+	{
+		// Limpiamos la ventana
+		App.clear();
+		//Dibujamos la escena
+		 // Mostramos la ventana
+		App.display();
+	}
+	return 0;
+}
